5db3d84d00c290d9328dc1abe4cfd1b7d28153eb
Jeff <j.je.jef.jeff@gmail.com>
2016-06-18 17:52:13 +0200

    no message

--- libgdx/desktop/src/org/bob/desktop/DesktopLauncher.java
+++ libgdx/desktop/src/org/bob/desktop/DesktopLauncher.java
@@ -2,14 +2,23 @@ package org.bob.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 import org.bob.core.Game;
+
+import static com.badlogic.gdx.tools.texturepacker.TexturePacker.process;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Game.width/2;
 		config.height = Game.height/2;
+
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxWidth = 512;
+		settings.maxHeight = 512;
+		process(settings, "gfx/texturepack", "sprite", "game");
+
 		new LwjglApplication(new Game(), config);
 	}
 }
