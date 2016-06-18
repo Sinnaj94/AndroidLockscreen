7303cd79108b20fcb4a771c13d37a109649b1d4d
Jeff <j.je.jef.jeff@gmail.com>
2016-06-18 14:07:42 +0200

    no message

--- libgdx/core/src/org/bob/core/Game.java
+++ libgdx/core/src/org/bob/core/Game.java
@@ -8,6 +8,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -18,16 +22,29 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  * Created by jeff on 16/06/16.
  */
 public class Game extends InputAdapter implements ApplicationListener {
+
+    private float accumulator = 0;
+    private float TIME_STEP = 1/300f;
+    private int VELOCITY_ITERATIONS = 6;
+    private int POSITION_ITERATIONS = 2;
+
     SpriteBatch batch;
     Viewport viewport;
     Camera camera;
     Bob bob;
     Stage stage;
+    World world;
+    Box2DDebugRenderer debugRenderer;
+
     private Array<Sprite> bears;
 
     @Override
     public void create() {
 
+        // Box2d
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
 
         camera = new PerspectiveCamera();
         viewport = new StretchViewport(1080, 1920, camera);
@@ -60,7 +77,22 @@ public class Game extends InputAdapter implements ApplicationListener {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+
+        debugRenderer.render(world, camera.combined);
+        doPhysicsStep(Gdx.graphics.getDeltaTime());
     }
+
+    private void doPhysicsStep(float deltaTime) {
+        // fixed time step
+        // max frame time to avoid spiral of death (on slow devices)
+        float frameTime = Math.min(deltaTime, 0.25f);
+        accumulator += frameTime;
+        while (accumulator >= TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            accumulator -= TIME_STEP;
+        }
+    }
+
 
     @Override
     public void pause() {
---
+++ libgdx/desktop/src/org/bob/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package org.bob.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import org.bob.core.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
