875ef8a802cb63fd075a226695a1b4ce45c9402c
Jeff <j.je.jef.jeff@gmail.com>
2016-06-20 00:05:17 +0200

    no message

--- libgdx/core/src/org/bob/core/Bob.java
+++ libgdx/core/src/org/bob/core/Bob.java
@@ -2,7 +2,6 @@ package org.bob.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -13,7 +12,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.MassData;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -339,7 +337,6 @@ public class Bob extends Actor {
         //Children Updates:
         p.updateParticles();
 
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         stateTime += Gdx.graphics.getDeltaTime();
         currentFrame = returnSpriteSheet();  // #16
         spriteBatch.begin();
