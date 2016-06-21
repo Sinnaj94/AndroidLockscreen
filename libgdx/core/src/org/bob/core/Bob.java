dfd07d56b76085d0665ffa88812ad5af81b26f83
Jeff <j.je.jef.jeff@gmail.com>
2016-06-21 22:27:25 +0200

    no message

--- libgdx/core/src/org/bob/core/Bob.java
+++ libgdx/core/src/org/bob/core/Bob.java
@@ -352,7 +352,7 @@ public class Bob extends Actor {
     @Override
     public void draw(Batch batch, float alpha) {
 
-        System.out.println(body.getLinearVelocity().x + " : " + body.getLinearVelocity().y);
+        //System.out.println(body.getLinearVelocity().x + " : " + body.getLinearVelocity().y);
 
         //Children Updates:
         p.updateParticles();
