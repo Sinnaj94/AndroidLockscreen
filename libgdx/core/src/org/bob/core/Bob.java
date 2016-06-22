b3e2111df44d944af91926b972358f889c57e1d6
Sinnaj94 <jannis.jahr@hotmail.de>
2016-06-23 00:52:57 +0200

    no message

--- libgdx/core/src/org/bob/core/Bob.java
+++ libgdx/core/src/org/bob/core/Bob.java
@@ -240,7 +240,7 @@ public class Bob extends Actor {
             return MathUtils.random(5f, 12f);
 
         } else if (currentAction == 5) {
-            return 3;
+            return 6f;
         }
         return MathUtils.random(5f, 10f);
 
